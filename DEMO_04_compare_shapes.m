1093ef77b76f5a6b03b2be8e31f51eda02b545e0
Bart <b.bolsterlee@neura.edu.au>
2021-08-13 10:39:08 +1000

    updates to surface bootstrapping

--- DEMO_04_compare_shapes.m
+++ DEMO_04_compare_shapes.m
@@ -25,20 +25,33 @@ results = load(fullfile(dataFolder,'registration','step02','registration_results
 % contains all coordinates (x,y and z) of the vertices of a shape.
 Xs = reshape(results.X,[],size(results.X,3));
 
-
 %% Bootstrap change in shape.
 % The effect of training is defined as the distance between corresponding
 % vertices before and after training. The sign of the distance is
 % determined by comparing the displacement vector with the normal
 % direction. If the projection of the displacement vector on the normal
-% vector is positive, the displacemtn is positive; otherwise it is
-% negative. This assumes that the normal vector to the surface points
-% outside, which needs to be verified.
+% vector is positive, the displacement is positive (local expansion: the
+% point after training is outside the surface before training); otherwise it is
+% negative (local contraction). This assumes that the normal vectors to the 
+% surface point outside, which needs to be verified.
 
-% Verify if the normal vector of the mean shape points outside.
+% Verify that the normal vector of the mean shape points outside.
 figure('Color','w')
 plotSurface(results.mean_shape,'ShowNormals',true)
 title('The surface normals should points OUTSIDE')
+view(0,15)
+
+% Bootstrap the surface to determine the significance of the effect of
+% training on local change in shape. In essence, the bootstrapping approach
+% replicates the study N times by randomly sampling (with replacement) from
+% the existing shapes. For each bootstrapping replicate, the signed change
+% in shape for all vertices is determined. This will give for each vertex
+% a distribution of N values for local changes in shape. If the 2.5th
+% percentile of this distribution is above 0, the local expansion in shape
+% is considered statistically significant. If the 97.5th percentile of the
+% distribution is below 0, the local contraction of the shape is considered
+% significant. Otherwise, it is not significant (i.e. 95th confidence
+% interval includes 0).
 
 bs_results = bootstrap_surface(Xs,...       % 2D array of all vertices (rows) of all shapes (column) - see bootstrap_surface.m for formatting
     results.mean_shape.ConnectivityList,... % list of connectivity (faces) of vertices in Xs
@@ -46,6 +59,7 @@ bs_results = bootstrap_surface(Xs,...       % 2D array of all vertices (rows) of
     'plot',true,...   % plot mean, lower bound, upper bound and significance map of change in shape
     'paired',true,... % set to true if the data are paired - make sure entries in group 1 and group 2 are corresponding
     'maxdist',5,...   % maxdist is just for plotting purposes
-    'N',100,...       % number of bootstrap replicates. If not provided, the default of 1000 is used.
+    'N',1000,...       % number of bootstrap replicates. If not provided, the default of 1000 is used.
     'align',true,...  % if true, all shapes are rigiidly aligned with Procrustes alignment before bootstrapping.
     'flipnormals',false); % set to true if the normal vectors are pointing inside.
+
--- src/bootstrap_surface.m
+++ src/bootstrap_surface.m
@@ -1,4 +1,4 @@
-function [avg,lb,ub,distr] = bootstrap_surface(X,conn,group1,group2,varargin)
+function results = bootstrap_surface(X,conn,group1,group2,varargin)
 %BOOTSTRAP_SURFACE Calculates the mean and confidence interval of the
 % distance between two groups of surfaces. The distance is defined as the
 % distance of the averaged surface of group 2 to the averaged surface of
@@ -6,7 +6,7 @@ function [avg,lb,ub,distr] = bootstrap_surface(X,conn,group1,group2,varargin)
 % lower/upper bounds.
 %
 % USAGE:
-% [avg,lb,ub,distr] = boostrap_surface(X,conn,group1,group2,varargin)
+% results = boostrap_surface(X,conn,group1,group2,varargin)
 %
 % INPUT
 % X   : m x n array of m corresponding points on a selection of n surfaces.
@@ -17,7 +17,6 @@ function [avg,lb,ub,distr] = bootstrap_surface(X,conn,group1,group2,varargin)
 % group 2 : mx1 indices of surfaces in group 2.
 %
 % Optional input arguments, provided as 'argument',<value> pairs:
-% padding : padding of the distance map around the shape. Default: 10 mm.
 % N      : number of samples from each group to draw to determine the 95%
 %          confidence intervals. Default: N=1000
 % paired : if true, the shapes in group 1 and group 2 are paired (e.g.,
@@ -27,10 +26,15 @@ function [avg,lb,ub,distr] = bootstrap_surface(X,conn,group1,group2,varargin)
 % plot   : if true, a figure with surface coloured by the mean, the lower
 %          bound, the upper bound, and a 'significance' (CI crosses 0).
 %          Default: true.
-% maxdist: maximum distance for the colorbar.
+% print  : if true, some summary statistics are printed to the screen.
+%          Default: true
+% maxdist: maximum distance for the colorbar (just for plotting purposes).
 % binsize: bin size for histogram. Default: 1
 % align  : if true, all shapes are rigidly aligned to the group mean using
 %          procrustes alignment
+% biascorr : if true, the mean of the sampling distribution is shifted to
+%            the mean of the original dataset to reduce sampling bias in
+%            the estimates of the confidence interval.
 % lb_pct : percentile for the lower bound of the confidence interval.
 %          Default: 2.5
 % ub_pct : percentile for the lower bound of the confidence interval.
@@ -42,10 +46,12 @@ function [avg,lb,ub,distr] = bootstrap_surface(X,conn,group1,group2,varargin)
 %              corresponding points). Default: false.
 %
 % OUTPUT
+% structure with the following fields:
 % avg    : m x 1 array with mean effect for all vertices
 % lb     : m x 1 array with lower bound for all vertices
 % ub     : m x 1 array with upper bound for all vertices
 % distr  : m x n array with sampling distribution for all vertices
+% ...and some options as described under INPUT
 %
 % Bart Bolsterlee, Neuroscience Research Australia (NeuRA)
 % July 2021
@@ -57,27 +63,36 @@ addRequired(p,'conn',@isnumeric)
 addRequired(p,'group1',@isnumeric)
 addRequired(p,'group2',@isnumeric)
 addParameter(p,'N',1000)
-addParameter(p,'padding',10)
 addParameter(p,'paired',false)
-addParameter(p,'plot',true)
-addParameter(p,'maxdist',[])
-addParameter(p,'binsize',1)
+addParameter(p,'biascorr',true)
 addParameter(p,'align',false)
 addParameter(p,'flipnormals',false)
+addParameter(p,'plot',true)
+addParameter(p,'print',true)
+addParameter(p,'maxdist',[])
+addParameter(p,'binsize',1)
 addParameter(p,'lb_pct',2.5,@isscalar)
 addParameter(p,'ub_pct',97.5,@isscalar)
 addParameter(p,'proj',false)
 parse(p,X,conn,group1,group2,varargin{:});
+
+
+results.options.N           = p.Results.N;   % number of bootstrap replicates
+results.options.biascorr    = p.Results.biascorr;
+results.options.align       = p.Results.align;
+results.options.flipnormals = p.Results.flipnormals;
+results.options.lb_pct      = p.Results.lb_pct;
+results.options.ub_pct      = p.Results.ub_pct;
+results.options.proj        = p.Results.proj;
+results.options.paired      = p.Results.paired;
+
+n1 = numel(group1); % number of shapes in group 1
+n2 = numel(group2); % number of shapes in group 2
     
-if p.Results.align == true
+if results.options.align == true
     % Pre-align all shapes to the mean shape using procrustes aligment.
     X = rigid_align_to_mean(X);
 end
-
-N     = p.Results.N; % number of samples
-distr = NaN(size(X,1)/3,N); % array with distances for all vertices and samples
-n1 = numel(group1); % number of shapes in group 1
-n2 = numel(group2); % number of shapes in group 2
 
 if p.Results.paired == true
     if n1 ~= n2
@@ -104,16 +119,17 @@ sgn = sign(sum(d_vec .* N1,2));
 if p.Results.proj == true
     % Magnitude and direction of projection of displacement vector on normal vector
     projection = d_vec .* N1; % projection of displacement vector on normal vector
-    avg = sgn .* sqrt(sum(projection.^2,2));
+    results.avg = sgn .* sqrt(sum(projection.^2,2));
 else
     % Magnitude and direction of displacement vector
-    avg = sgn .* sqrt(sum(d_vec.^2,2));
+    results.avg = sgn .* sqrt(sum(d_vec.^2,2));
 end
 
-hwait = waitbar(0,'','Name','Sampling surfaces and interpolating distance maps');
-for ii = 1 : N
-    waitbar(ii/N,hwait,sprintf('%d of %d',ii,N))
-    if p.Results.paired == true % paired samples
+hwait = waitbar(0,'','Name','Bootstrapping surface');
+results.distr = NaN(size(X,1)/3,results.options.N); % array with distances for all vertices and samples
+for ii = 1 : results.options.N
+    waitbar(ii/results.options.N,hwait,sprintf('%d of %d',ii,results.options.N))
+    if results.options.paired == true % paired samples
         % Draw a random sample of indices from 1 to n1 (number of shapes
         % per group)
         random_sample = datasample(1:n1,n1);
@@ -138,29 +154,46 @@ for ii = 1 : N
     d_vec = TR2.Points - TR1.Points;
     N1 = vertexNormal(TR1);
     sgn = sign(sum(d_vec .* N1,2));
-    if p.Results.proj == true
+    if results.options.proj == true
         % Magnitude and direction of projection of displacement vector on normal vector
         projection = d_vec .* N1;
-        distr(:,ii) = sgn .* sqrt(sum(projection.^2,2));
+        results.distr(:,ii) = sgn .* sqrt(sum(projection.^2,2));
     else
         % Magnitude and direction of displacement vector
-        distr(:,ii) = sgn .* sqrt(sum(d_vec.^2,2));
+        results.distr(:,ii) = sgn .* sqrt(sum(d_vec.^2,2));
     end
 end
 close(hwait)
 fprintf('\n')
 
+if results.options.biascorr == true
+    % Bias correction: correct sampling distribution to have a mean equal to
+    % the true mean
+    results.bias = nanmean(results.distr,2) - results.avg;
+    results.distr = results.distr - results.bias;
+    results.avg = results.avg;
+end
 % Calculate lower and upper bound of the 95% confidence interval. Correct
 % for small interpolation errors by subtracting the distance (d0) of the mean
 % shape to the distance map. d0 should be zero if the distance map is
 % perfect.
-lb = prctile(distr,p.Results.lb_pct,2); % lower bound
-ub = prctile(distr,p.Results.ub_pct,2); % upper bound
+results.lb = prctile(results.distr,results.options.lb_pct,2); % lower bound
+results.ub = prctile(results.distr,results.options.ub_pct,2); % upper bound
+
+if p.Results.print == true
+    % Print results to the screen
+    fprintf(' -------- BOOTSTRAP RESULTS --------\n')
+    fprintf('Average expansion (SD)                   : %.1f (%.1f)\n',mean(results.avg),std(results.avg))
+    fprintf('Significant contraction (%% of vertices) : %.2f \n',sum(results.ub<0)/numel(results.lb)*100)
+    fprintf('Not significant         (%% of vertices) : %.2f \n',sum(results.lb<0 & results.ub>0)/numel(results.lb)*100)
+    fprintf('Significant expansion   (%% of vertices) : %.2f \n',sum(results.lb>0)/numel(results.lb)*100)
+end
+
 
 %% Plot the results
 if p.Results.plot == true
     if isempty(p.Results.maxdist)
-        maxdist = ceil(max(abs(distr(:))));
+        maxdist = ceil(max(abs(results.distr(:))));
     else
         maxdist = p.Results.maxdist;
     end
@@ -180,27 +213,27 @@ if p.Results.plot == true
             'SpecularStrength',0.1);
         switch i
             case 1 % mean difference
-                cdata = avg;
+                cdata = results.avg;
                 set(hp,'FaceVertexCData',cdata)
                 titleTxt = 'mean surface-to-surface distance';
                 cmap = redblueTecplot(100);
                 set(gca,'CLim',[-1 1]*maxdist)
             case 2 % lower boundary (2.5 percentile)
-                cdata = lb;
+                cdata = results.lb;
                 set(hp,'FaceVertexCData',cdata)
                 titleTxt = 'lower bound';
                 cmap = redblueTecplot(100);
                 set(gca,'CLim',[-1 1]*maxdist)
             case 3 % upper boundary (97.5 percentile)
-                cdata = ub;
+                cdata = results.ub;
                 set(hp,'FaceVertexCData',cdata)
                 titleTxt = 'upper bound';
                 cmap = redblueTecplot(100);
                 set(gca,'CLim',[-1 1]*maxdist)
             case 4 % CI includes 0, >0 or <0
-                cdata = ones(size(lb))*1;
-                cdata(ub < 0) = 0; % upper bound < 0 - significant negative change
-                cdata(lb > 0) = 2; % lower bound > 0 - significant positive change
+                cdata = ones(size(results.lb))*1;
+                cdata(results.ub < 0) = 0; % upper bound < 0 - significant negative change
+                cdata(results.lb > 0) = 2; % lower bound > 0 - significant positive change
                 %                 cdata = double(lb <= 0 & ub >= 0);
                 set(hp,'FaceVertexCData',cdata)
                 %                 titleTxt = 'yellow = significant (CI does not include 0)';
@@ -222,7 +255,7 @@ if p.Results.plot == true
         xlim = get(gca,'Xlim');
         ylim = get(gca,'Ylim');
         zlim = get(gca,'Zlim');
-        padding = 15;
+        padding = 5;
         axis(gca,[xlim+[-1 1]*padding ylim+[-1 1]*padding zlim+[-1 1]*padding])
         title(titleTxt)
         
